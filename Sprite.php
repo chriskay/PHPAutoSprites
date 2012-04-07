e57d5950a02cc6a6a4ced492d457bf91aeba9b88
ChrisKay <glowhand@gmail.com>
2012-04-07 23:13:32 +0200

    Exported sprite creation logics into another class, improved algorithm

---
+++ Sprite.php
@@ -0,0 +1,462 @@
+<?php
+require_once('Exception.php');
+
+abstract class Sprite
+{
+        /* Configuration */
+        const IMAGE_DIR = '../images'; // path to image dir relative to directory of sprite.php
+        const SPRITE_DIR = 'sprites'; // path where sprites should be saved relative to IMAGE_DIR
+        const SPRITE_URL = 'images/sprites/'; // URL to sprites , eg. http://mywebsite.com/images/sprites
+
+        const DATA_DIR = 'data'; // path to data dir relative to directory of sprite.php
+        const TRACK_FILE = 'tracks.json'; // name of track file in DATA_DIR
+        const SPRITE_FILE = 'sprites.json'; // name of sprite info file in DATA_DIR         
+
+        const PNG_COMPRESSION = 9; // how much to compress sprite pngs from 0 (no compression) to 9
+        const MAX_SPRITE_PIXEL = 250000; // maximal sum of width*height of all images
+
+        protected static $pseudo_classes = array(
+                'active', 'hover'
+        );
+
+        /**
+         * creates image HTML element with CSS sprite data
+         * @param type $src source of image by default (no :hover etc.)
+         * @param type $attributes HTML attributes, eg. array('alt' => 'img alt text')
+         * @param type $sprite_nr number of sprite image should be placed in or -1 for auto assignment
+         * @return string HTML element
+         * @throws SpriteException 
+         */
+        public static function image($src, $attributes = array(), $sprite_nr = -1)
+        {
+                return self::htmlElement('div', $src, $attributes, $sprite_nr);
+        }
+
+        /**
+         * creates image input HTML element with CSS sprite data
+         * @param type $src source of image by default (no :hover etc.)
+         * @param type $attributes HTML attributes, eg. array('alt' => 'img alt text')
+         * @param type $sprite_nr number of sprite image should be placed in or -1 for auto assignment
+         * @return string HTML element
+         * @throws SpriteException 
+         */
+        public static function input($src, $attributes = array(), $sprite_nr = -1)
+        {
+                if(!isset($attributes['type']))
+                {
+                        $attributes['type'] = 'submit';
+                }
+
+                if(!isset($attributes['value']))
+                {
+                        $attributes['value'] = '';
+                }
+
+                if(!isset($attributes['style']))
+                {
+                        $attributes['style'] = 'border-width: 0px;';
+                }
+
+                return self::htmlElement('input', $src, $attributes, $sprite_nr);
+        }
+
+        protected static $sprite_existing = true;
+        
+        /**
+         * creates HTML element with CSS sprite data
+         * @param type $element tag name of element eg. 'div'
+         * @param type $src source of image by default (no :hover etc.)
+         * @param type $attributes HTML attributes, eg. array('alt' => 'img alt text')
+         * @param type $sprite_nr number of sprite image should be placed in or -1 for auto assignment
+         * @return string HTML element
+         * @throws SpriteException 
+         */
+        public static function htmlElement($element, $src, $attributes = array(), $sprite_nr = -1)
+        {
+                $src = __DIR__.DIRECTORY_SEPARATOR.self::IMAGE_DIR.DIRECTORY_SEPARATOR.$src;
+                
+                self::trackImage($src, $sprite_nr);                        
+
+                $sprite = self::getSpriteData($src);
+
+                $html = '<'.$element;
+
+                if(!$sprite)
+                {
+                        self::createSprites();
+
+                        $sprite = self::getSpriteData($src);
+                        
+                        if(!$sprite) // image not existing
+                        {
+                                throw new SpriteException('Image '.$src.' could not be loaded.');
+                        }
+                        
+                        $html .= ' style="'.addslashes(self::getCSS($src)).'"';
+
+                        self::$sprite_existing = false;
+                }
+                else
+                {
+                        $html .= ' class="'.self::getClassName($src).'"';
+
+                        if(!self::$sprite_existing)
+                        {
+                                if(isset($attributes['style']))
+                                {
+                                        $attributes['style'] .= self::getCSS($src);
+                                }
+                                else
+                                {
+                                        $attributes['style'] = self::getCSS($src);
+                                }
+                        }
+                }
+
+                foreach($attributes as $key => $attribute)
+                {
+                        $html .= ' '.$key.'="'.$attribute.'"';
+                }
+
+                $html .= '></'.$element.'>';
+
+                return $html;
+        }
+
+        /**
+         * save image source and sprite nr in track file
+         * @param  $src image source
+         * @param int $sprite_nr number of sprite or -1 if a sprite should 
+         * be automatically assigned
+         */
+        protected static function trackImage($src, $sprite_nr = -1)
+        {
+                $tracks = self::getFileContent(self::TRACK_FILE);
+
+                $tracks[$src] = $sprite_nr;
+                self::setFileContent(self::TRACK_FILE, $tracks);
+        }
+
+        /**
+         * get info about sprite position of an image via it's source
+         * @param string $src image source
+         * @return mixed sprite info array or false if no info exist
+         */
+        protected static function getSpriteData($src)
+        {
+                $sprites = self::getFileContent(self::SPRITE_FILE);
+
+                if(isset($sprites[$src]))
+                {
+                        return $sprites[$src];
+                }
+
+                return false;
+        }
+
+        /**
+         * creates CSS sprite images
+         * @throws SpriteException 
+         */
+        protected static function createSprites()
+        {
+                $counts = self::getFileContent(self::TRACK_FILE);
+                $sprites = array();
+
+                $pseudo_classes = array_merge(array(''), self::$pseudo_classes);
+
+                $images = array();
+                foreach($counts as $source => $sprite_nr)
+                {
+                        foreach($pseudo_classes as $pseudoClass)
+                        {
+                                $src = self::getSrcWithPseudoClass($source, $pseudoClass);
+                                if(!is_readable($src))
+                                {
+                                        if(isset($counts[$src]))
+                                        {
+                                                unset($counts[$src]);
+                                        }
+                                        continue;
+                                }
+
+                                $images[$src] = $sprite_nr;
+                        }
+                }
+
+                // assign images to sprites
+                $sprites = array();
+                $index = 0;
+                foreach($images as $src => $sprite_nr)
+                {
+                        $i = $sprite_nr;
+                        if($sprite_nr < 0)
+                        {
+                                $i = 'auto'.$index;
+                                
+                                if(isset($sprites[$i]) && $sprites[$i]->getPixelSum() > self::MAX_SPRITE_PIXEL)
+                                {
+                                        $index++;
+                                        $i = 'auto'.$index;
+                                }
+                        }
+                        
+                        if(!isset($sprites[$i]))
+                        {
+                                $sprites[$i] = new SpriteCreator();       
+                        }
+                        
+                        $sprites[$i]->addImage($src);
+                }
+
+                self::deleteSprites();
+                $positions = array();
+                foreach($sprites as $sprite)
+                {
+                        $spriteFileName = md5(time().microtime());
+
+                        $sprite->output(__DIR__.DIRECTORY_SEPARATOR.self::IMAGE_DIR.DIRECTORY_SEPARATOR.self::SPRITE_DIR.DIRECTORY_SEPARATOR.$spriteFileName.'.png', self::PNG_COMPRESSION);
+                
+                        foreach($sprite->positions() as $position)
+                        {
+                                $src = $position['src'];
+                                unset($position['src']);
+                                
+                                $position['sprite'] = $spriteFileName;
+                                $positions[$src] = $position;
+                        }
+                }
+                
+                self::setFileContent(self::SPRITE_FILE, $positions);
+        }
+
+        /**
+         * @var array remembers file content of files. So getFileContent
+         * needs to open file just once.
+         */
+        protected static $file_memorization = array();
+        
+        /**
+         * get content from file
+         * @param string $file name of file in DATA_DIR
+         * @return array content
+         */
+        protected static function getFileContent($file)
+        {
+                if(!isset(self::$file_memorization[$file]))
+                {
+                        $arr = array();
+                        $file = __DIR__.DIRECTORY_SEPARATOR.self::DATA_DIR.DIRECTORY_SEPARATOR.$file;
+
+                        if(is_readable($file))
+                        {
+                                $content = file_get_contents($file);
+                                $arr = json_decode($content);
+
+                                if($arr === null)
+                                {
+                                        $arr = array();
+                                        unlink($file);
+                                }
+
+                                $arr = (array) $arr;
+
+                                foreach($arr as $key => $val)
+                                {
+                                        if(is_object($val))
+                                        {
+                                                $arr[$key] = (array) $val;
+                                        }
+                                }
+                        }
+
+
+                        self::$file_memorization[$file] = $arr;
+                }
+
+                return self::$file_memorization[$file];
+        }
+
+        /**
+         * saves content array json encoded in specified file
+         * @param string $file
+         * @param array $content 
+         */
+        protected static function setFileContent($file, array $content)
+        {
+                $file = __DIR__.DIRECTORY_SEPARATOR.self::DATA_DIR.DIRECTORY_SEPARATOR.$file;
+                $handle = fopen($file, 'w+');
+
+                fwrite($handle, json_encode($content));
+                fclose($handle);
+
+                self::$file_memorization[$file] = $content;
+        }
+
+        /**
+         * delete all sprites images created before that are older than 1 minute
+         */
+        protected static function deleteSprites()
+        {
+                $sprite_dir = __DIR__.DIRECTORY_SEPARATOR.self::IMAGE_DIR.DIRECTORY_SEPARATOR.self::SPRITE_DIR;
+                $handle = opendir($sprite_dir);
+                while(($file = readdir($handle)) !== false)
+                {
+                        if($file{0} != '.')
+                        {
+                                $modified = filemtime($sprite_dir.DIRECTORY_SEPARATOR.$file);
+
+                                if($modified < (time()-60))
+                                {
+                                        unlink($sprite_dir.DIRECTORY_SEPARATOR.$file);
+                                }
+                        }
+                }
+        }
+
+        /**
+         * returns complete <style> tag with all the CSS data for all sprite images
+         * @return string <style> tag with CSS data
+         */
+        public static function style()
+        {
+                $html = '<style type="text/css">';
+
+                $sprites = self::getFileContent(self::SPRITE_FILE);
+                foreach($sprites as $src => $sprite)
+                {
+                        $html .= '.'.self::getClassName($src);
+
+                        $pseudoClass = self::getPseudoClass($src);
+                        if($pseudoClass)
+                        {
+                                $html .= ':'.$pseudoClass;
+                        }
+
+                        $html .= '{'.self::getCSS($src).'}';
+                }
+
+                $html .= '</style>';
+
+                return $html;
+        }
+
+        /**
+         * return CSS class name for image source
+         * @param string $src image source
+         * @return string CSS class name
+         */
+        protected static function getClassName($src)
+        {
+                $hash = md5(self::getSrcWithoutPseudoClass($src));
+                $hash = 'sprite_'.substr($hash, 0, 10);
+                return $hash;
+        }
+
+        /**
+         * returns CSS data for image defined by source
+         * @param string $src image source
+         * @return string minified CSS code 
+         */
+        protected static function getCSS($className)
+        {
+                $sprite = self::getSpriteData($className);
+                $css = '';
+                if($sprite)
+                {
+                        $css =  'display: inline-block;
+                                background-image: url('.self::SPRITE_URL.$sprite['sprite'].'.png);
+                                background-position: -'.$sprite['x'].'px -'.$sprite['y'].'px;
+                                background-color: transparent;
+                                width: '.$sprite['width'].'px;
+                                height: '.$sprite['height'].'px';
+                }
+
+                $css = str_replace(array(
+                        "\r",
+                        "\n",
+                        '  '
+                ), '', $css);
+
+                return $css;
+        }
+
+        /**
+         * adds pseudo class to file name
+         * @param string $src image source, eg. 'images/img.png'
+         * @param string $pseudoClass name of pseudo class, eg. 'hover'
+         * @return string file name with pseudo class, eg. 'images/img_hover.png' 
+         */
+        protected function getSrcWithPseudoClass($src, $pseudoClass)
+        {
+                if(empty($pseudoClass))
+                {
+                        return $src;
+                }
+
+                // extends _pseudo class to image file name
+                $src_exploded = explode('/', $src);
+                $file_name_ext = $src_exploded[count($src_exploded)-1];
+
+                $file_exploded = explode('.', $file_name_ext);
+                $file_name = $file_exploded[count($file_exploded)-2];
+                $file_ext = $file_exploded[count($file_exploded)-1];
+
+                $file_name_ps = null;
+                if(empty($pseudoClass))
+                {
+                        $file_name_ps = $file_name.$pseudoClass.'.'.$file_ext;
+                }
+                else
+                {
+                        $file_name_ps = $file_name.'_'.$pseudoClass.'.'.$file_ext;
+                }
+
+                $src_exploded[count($src_exploded)-1] = $file_name_ps;
+                $src = implode('/', $src_exploded);
+
+                return $src;
+        }
+
+        /**
+         * get file name without pseudo class in it
+         * @param string $src eg. 'images/img_hover.png'
+         * @return string eg. 'images/img.png'
+         */
+        protected function getSrcWithoutPseudoClass($src)
+        {                        
+                $src_exploded = explode('/', $src);
+                $file_name_ext = $src_exploded[count($src_exploded)-1];
+
+                foreach(self::$pseudo_classes as $pseudoClass)
+                {
+                        $file_name_ext = str_replace('_'.$pseudoClass, '', $file_name_ext);
+                }
+
+                $src_exploded[count($src_exploded)-1] = $file_name_ext;
+                $src = implode('/', $src_exploded);
+
+                return $src;
+        }
+
+        /**
+         * get pseudo class from file name
+         * @param type $src eg. 'images/image_hover.png'
+         * @return mixed pseudo class or false if no pseudo class was found, eg. 'hover'
+         */
+        protected function getPseudoClass($src)
+        {
+                $src_exploded = explode('/', $src);
+                $file_name_ext = $src_exploded[count($src_exploded)-1];
+
+                foreach(self::$pseudo_classes as $pseudoClass)
+                {
+                        if(strpos($file_name_ext, '_'.$pseudoClass))
+                        {
+                                return $pseudoClass;
+                        }
+                }
+
+                return false;
+        }
+}
---
+++ SpriteCreator.php
@@ -0,0 +1,195 @@
+<?php
+class SpriteCreator
+{
+        protected $images = array();
+        
+        public function addImage($image_src)
+        {
+                $info = getimagesize($image_src);
+                if(!$info)
+                {
+                        throw new SpriteException('Could not read '.$image_src.'.');
+                        return;
+                }
+                
+                $image = null;
+                switch($info['mime'])
+                {
+                        case image_type_to_mime_type(IMAGETYPE_JPEG):
+                                $image = imagecreatefromjpeg($image_src);
+                                break;
+                        case image_type_to_mime_type(IMAGETYPE_PNG):
+                                $image = imagecreatefrompng($image_src);
+                                break;
+                        case image_type_to_mime_type(IMAGETYPE_GIF):
+                                $image = imagecreatefromgif($image_src);
+                                break;
+                }
+                
+                if(!$image)
+                {
+                        throw new SpriteException('Unsupported mime type of '.$image_src.'.');
+                        return;
+                }
+                
+                // this key is necessary for sorting images by width
+                $key = sprintf('%09d',$info[0]).sprintf('%04d',rand(0,9999));
+                $this->images[$key] = array(
+                    'resource' => $image,
+                    'src' => $image_src);
+        }
+        
+        public function getSpriteData()
+        {
+                if(count($this->images) < 1)
+                {
+                        throw new SpriteException('No images added.');
+                        return;
+                }
+                
+                // sort images by width, biggest first
+                krsort($this->images);
+                
+                // sprite width = biggest width of all images
+                $biggest = array_shift($images = $this->images);
+                $spriteWidth = imagesx($biggest['resource']);
+                $spriteHeight = 0;
+                
+                // $freeBlocks will save top left x and y coordinates, width and height, where image still can be placed
+                $freeBlocks = array(
+                    0 => array(
+                        'x' => 0,
+                        'y' => 0,
+                        'height' => 0, // height = 0 means, block is at the bottom of the sprite image
+                        'width' => $spriteWidth
+                    )
+                );
+                
+                // $imagePositions will save where each image will be placed in div (x,y)
+                $imagePositions = array();
+                
+                // get image positions
+                foreach($this->images as $arr)
+                {
+                        $image = $arr['resource'];
+                        
+                        $chosenBlockKey = null;
+                        $chosenBlock = null;
+                        foreach($freeBlocks as $key => $block)
+                        {
+                                // check if image fits in block
+                                if($block['width'] >= imagesx($image) &&
+                                        ($block['height'] >= imagesy($image) || $block['height'] == 0))
+                                {
+                                        // check if this block is better than chosen block before
+                                        if($chosenBlock == null ||
+                                                ($chosenBlock['height'] == 0 || $chosenBlock['width'] > $block['width']))
+                                        {
+                                                $chosenBlockKey = $key;
+                                                $chosenBlock = $block;
+                                        }
+                                }
+                        }
+                        
+                        // remove block from freeBlocks array
+                        unset($freeBlocks[$chosenBlockKey]);
+                        
+                        // if this block has no height (= is at the bottom of sprite image), increase sprite height
+                        if($chosenBlock['height'] == 0)
+                        {
+                                $spriteHeight += imagesy($image);
+                                
+                                // add new block under this image (sprite bottom)
+                                $freeBlocks[] = array(
+                                    'x' => 0,
+                                    'y' => $chosenBlock['y']+imagesy($image),
+                                    'height' => 0,
+                                    'width' => $spriteWidth
+                                );
+                        }
+                        
+                        // add block under this image
+                        elseif((imagesy($image)+$chosenBlock['y']) < $spriteHeight)
+                        {
+                                $freeBlocks[] = array(
+                                    'x' => $chosenBlock['x'],
+                                    'y' => $chosenBlock['y']+imagesy($image),
+                                    'height' => $chosenBlock['height']-imagesy($image),
+                                    'width' => $spriteWidth
+                                );
+                        }
+                       
+                        // add block right behind this image
+                        if(($chosenBlock['x']+imagesx($image)) < $spriteWidth)
+                        {
+                                $freeBlocks[] = array(
+                                    'x' => $chosenBlock['x']+imagesx($image),
+                                    'y' => $chosenBlock['y'],
+                                    'height' => imagesy($image)-1,
+                                    'width' => $spriteWidth-($chosenBlock['x']+imagesx($image))
+                                );
+                        }
+                        
+                        $imagePositions[] = array(
+                            'image' => $image,
+                            'src' => $arr['src'],
+                            'x' => $chosenBlock['x'],
+                            'y' => $chosenBlock['y'],
+                            'width' => imagesx($image),
+                            'height' => imagesy($image)
+                        );
+                }
+                
+                return array(
+                    'height' => $spriteHeight,
+                    'width' => $spriteWidth,
+                    'positions' => $imagePositions
+                );
+        }
+        
+        public function getPixelSum()
+        {
+                $sum = 0;
+                foreach($this->images as $image)
+                {
+                        $area = imagesx($image['resource'])*imagesy($image['resource']);
+                        $sum += $area;
+                }
+                
+                return $sum;
+        }
+        
+        public function positions()
+        {
+                $spriteData = $this->getSpriteData();
+                foreach($spriteData['positions'] as $key => $data)
+                {
+                        unset($data['image']);
+                        $spriteData['positions'][$key] = $data;
+                }
+                return $spriteData['positions'];
+        }
+        
+        public function output($fileName)
+        {
+                $spriteData = $this->getSpriteData();
+                
+                $sprite = imagecreatetruecolor($spriteData['width'],$spriteData['height']);
+                
+                foreach($spriteData['positions'] as $position)
+                {
+                        $x = $position['x'];
+                        $y = $position['y'];
+                        $image = $position['image'];
+                        $width = imagesx($image);
+                        $height = imagesy($image);
+                        imagecopyresampled($sprite, $image, $x, $y, 0, 0, $width, $height, $width, $height);
+                }
+                
+                
+                
+                imagepng($sprite, $fileName);
+                
+        }
+}
+?>
